e5603dc402af2ac6d3a103278f54da82ae450577
Alice <alicehata9@gmail.com>
2025-09-05 17:35:10 -0300

    enabled cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express"
 import { routes } from "@/routes"
 import { errorHandling } from "./middlewares/error-handling"
+import cors from "cors"
 
 const app = express()
-
+app.use(cors())
 app.use(express.json())
+
 app.use(routes)
 
 app.use(errorHandling)
